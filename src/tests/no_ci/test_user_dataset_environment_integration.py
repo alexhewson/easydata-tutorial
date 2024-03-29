b262e326d270b5c61da91db7e022eef5a976ce6e
Alex Hewson <alex.hewson@prisonreformtrust.org.uk>
2024-03-29 19:01:29 +0000

    test result files

--- src/tests/no_ci/test_user_dataset_environment_integration.py
+++ src/tests/no_ci/test_user_dataset_environment_integration.py
@@ -19,7 +19,7 @@ class TestDatasets(unittest.TestCase):
         ds = Dataset.load('penguins-clean')
         ### There is a typo in this test and it will fail.
         ### Please fix it!
-        assert ds.data.shape == (334, 4)
+        assert ds.data.shape == (334, 7)
 
     def test_penguins_scaled(self):
         ds = Dataset.load('penguins-scaled')
