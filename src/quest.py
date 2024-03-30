ccf56350b940bff496ed2f3fcb19637447581408
Alex Hewson <alex.hewson@prisonreformtrust.org.uk>
2024-03-30 15:30:57 +0000

    adding docstring

--- src/quest.py
+++ src/quest.py
@@ -1,2 +1,3 @@
 def quest_instruction():
+    '''This function returns a string with a suggested filename for your notebook'''
     print("Rename this notebook. `06-Story-Challenge` is a good option ;)")
